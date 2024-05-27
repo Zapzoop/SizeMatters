052ba50ccf25f4663eb814dfc82c4153c3eb4866
Zapzoop <turtlemaster700s@gmail.com>
2024-05-27 15:03:58 +0530

    build system in pace

--- levels/gridsystem.gd
+++ levels/gridsystem.gd
@@ -1,34 +1,48 @@
 extends TileMap
 
+var canvas_layer
+var mouse
+
 var indicator = preload("res://gui/tile_selector/tile_selector.tscn")
+var build_menu = preload("res://gui/build_menu/build_menu.tscn")
 
 var mouse_pos:Vector2
 var tile_pos:Vector2i
 var previous_tile_pos:Vector2i
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	#set_cell(0,Vector2i(0,0),2,Vector2i(0,0),1)
-	#set_cell(0,Vector2i(0,0),0,Vector2i(1,1),0)
-	pass
-	
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
+var locked_tile_pos:Vector2i
+var is_locked = false
+
+func _ready():
+	Global.current_tilemap = self
+	pass
+
 func _process(delta):
-	#mouse_pos = get_global_mouse_position()
-	#var tile_x = int(mouse_pos.x / 16)
-	#var tile_y = int(mouse_pos.y / 16)
-	#tile_pos = Vector2i(tile_x,tile_y)
+	if !is_locked:
+		tile_pos = local_to_map(get_global_mouse_position())
+		
+		if tile_pos != previous_tile_pos or previous_tile_pos == null:
+			erase_cell(1,previous_tile_pos)
+			set_cell(1,tile_pos,2,Vector2i(0,0),1)
+		
+		previous_tile_pos = tile_pos
 	
-	tile_pos = local_to_map(get_global_mouse_position())
-	
-	if tile_pos != previous_tile_pos or previous_tile_pos == null:
-		erase_cell(1,previous_tile_pos)
-		set_cell(1,tile_pos,2,Vector2i(0,0),1)
-	
-	previous_tile_pos = tile_pos
-	
-	#
-	#print(get_cell_source_id(0,tile))
-	#print(get_cell_atlas_coords(0,tile))
-	#print(get_cell_alternative_tile(0,tile))
-	
+func _input(event):
+	if event is InputEventMouseButton:
+		if event.button_index == MOUSE_BUTTON_LEFT and event.is_pressed() and is_locked == false:
+			is_locked = true
+			locked_tile_pos = tile_pos
+			var ins = build_menu.instantiate()
+			canvas_layer.add_child(ins)
+			Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+			mouse.hide()
+
+
+func place_block(vect:Vector2i):
+	set_cell(0,tile_pos,0,vect,0)
+	release()
+
+func release():
+	Input.set_mouse_mode(Input.MOUSE_MODE_HIDDEN)
+	mouse.show()
+	is_locked = false
--- levels/level_1.gd
+++ levels/level_1.gd
@@ -3,6 +3,8 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	$TileMap.canvas_layer = $CanvasLayer
+	$TileMap.mouse = $Mouse
 	print("level 1")
-
+	
 
