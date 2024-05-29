dad31f75b80bcd21d21d5f5ee1a4b8c64b861e26
Zapzoop <turtlemaster700s@gmail.com>
2024-05-29 22:03:13 +0530

    Added tile detection to fan

--- mechanics/fan/fan.gd
+++ mechanics/fan/fan.gd
@@ -3,6 +3,9 @@ extends Mechanics
 @export var fan_on:bool = false
 @export var fan_range:int = 3
 
+@onready var left_area = $range_left
+@onready var right_area = $range_right
+
 var tilemap
 var direction
 
@@ -24,6 +27,39 @@ func check():
 	if tilemap.get_cell_tile_data(0,left_one) == null and tilemap.get_cell_tile_data(0,right_one) == null:
 		self.queue_free()
 		Global.reset_build_menu()
+	elif tilemap.get_cell_tile_data(0,left_one) != null and tilemap.get_cell_tile_data(0,right_one) == null:
+		var left_side_block_value = tilemap.get_cell_tile_data(0,left_one).get_custom_data("can_attach")
+		if left_side_block_value == true:
+			direction = "right"
+			left_area.process_mode = Node.PROCESS_MODE_DISABLED
+			Global.reset_build_menu()
+		else:
+			self.queue_free()
+			Global.reset_build_menu()
+	elif tilemap.get_cell_tile_data(0,left_one) == null and tilemap.get_cell_tile_data(0,right_one) != null:
+		var right_side_block_value = tilemap.get_cell_tile_data(0,right_one).get_custom_data("can_attach")
+		if right_side_block_value == true:
+			direction = "left"
+			right_area.process_mode = Node.PROCESS_MODE_DISABLED
+			Global.reset_build_menu()
+		else:
+			self.queue_free()
+			Global.reset_build_menu()
+	elif tilemap.get_cell_tile_data(0,left_one) != null and tilemap.get_cell_tile_data(0,right_one) != null:
+		var left_side_block_value = tilemap.get_cell_tile_data(0,left_one).get_custom_data("can_attach")
+		var right_side_block_value = tilemap.get_cell_tile_data(0,right_one).get_custom_data("can_attach")
+		if left_side_block_value == true and  right_side_block_value == false:
+			direction = "right"
+			left_area.process_mode = Node.PROCESS_MODE_DISABLED
+			Global.reset_build_menu()
+		elif left_side_block_value == false and  right_side_block_value == true:
+			direction = "left"
+			right_area.process_mode = Node.PROCESS_MODE_DISABLED
+			Global.reset_build_menu()
+		elif left_side_block_value == true and  right_side_block_value == true or left_side_block_value == false and  right_side_block_value == false:
+			direction = "left"
+			right_area.process_mode = Node.PROCESS_MODE_DISABLED
+			Global.reset_build_menu()
 
 func _process(delta):
 	if fan_on:
